7967249941f82786bf79ed45b8122910b9e635de
mawalou14 <awaloum213@gmail.com>
2024-05-01 02:08:44 +0100

    Created the ApplicationDbContext

---
+++ ServerLibrary/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BaseLibary.Entities;
+using Microsoft.EntityFrameworkCore;
+namespace ServerLibrary.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<GeneralDepartment> GeneralDepartments { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Branch> Branches { get; set; }
+        public DbSet<Town> Towns { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
