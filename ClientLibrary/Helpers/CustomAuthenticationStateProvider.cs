8f874fccce3855350d220e627af7cdfac1c8c771
mawalou14 <awaloum213@gmail.com>
2024-05-01 23:45:51 +0100

    Still Creating CustomAuthenticationStateProvider

--- ClientLibrary/Helpers/CustomAuthenticationStateProvider.cs
+++ ClientLibrary/Helpers/CustomAuthenticationStateProvider.cs
@@ -1,12 +1,39 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
+﻿using BaseLibary.DTOs;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace ClientLibrary.Helpers
 {
-    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    public class CustomAuthenticationStateProvider(LocalStorageService localStorageService) : AuthenticationStateProvider
     {
-        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        private readonly ClaimsPrincipal anonymous = new(new ClaimsIdentity());
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            throw new NotImplementedException();
+            var stringToken = await localStorageService.GetToken();
+            if (string.IsNullOrEmpty(stringToken)) return await Task.FromResult(new AuthenticationState(anonymous));
+
+            var deserializeToken = Serializations.DeserializeJsonString<UserSession>(stringToken);
+            if (deserializeToken == null) return await Task.FromResult(new AuthenticationState(anonymous));
+
+            var getUserClaims = DescryptToken(deserializeToken.Token!);
+            if (getUserClaims == null) return await Task.FromResult(new AuthenticationState(anonymous));
+
+            var claimsPrincipal = SetClaimPrincipal(getUserClaims);
+            return await Task.FromResult(new AuthenticationState(claimsPrincipal));
+        }
+
+        private static CustomUserClaims DecryptToken(string jwtToken)
+        {
+            if (string.IsNullOrEmpty(jwtToken)) return new CustomUserClaims();
+
+            var handler = new JwtSecurityTokenHandler();
+            var token = handler.ReadJwtToken(jwtToken);
+            var userId = token.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+            var name = token.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Name);
+            var email = token.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email);
+            var role = token.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Role);
+            return new CustomUserClaims(userId!.Value, name!.Value, email!.Value, role!.Value);
         }
     }
 }
