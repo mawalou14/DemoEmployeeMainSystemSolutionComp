e09e72dfe8de9061f4606ed77c57b77f4cd5b063
mawalou14 <awaloum213@gmail.com>
2024-05-01 22:41:55 +0100

    Created IUserAccount interface

---
+++ ClientLibrary/Services/Contracts/IUserAccountService.cs
@@ -0,0 +1,14 @@
+﻿using BaseLibary.DTOs;
+using BaseLibary.Responses;
+using BaseLibrary.DTOs;
+
+namespace ClientLibrary.Services.Contracts
+{
+    public interface IUserAccountService
+    {
+        Task<GeneralResponse> CreateAsync(Register user);
+        Task<LoginResponse> SignInAsync(Login user);
+        Task<LoginResponse> RefreshTokenAsync(RefreshToken token);
+        Task<WeatherForecast[]> GetWeatherForecast();
+    }
+}
---
+++ ClientLibrary/Services/Implementations/UserAccountService.cs
@@ -0,0 +1,31 @@
+﻿using BaseLibary.DTOs;
+using BaseLibary.Responses;
+using BaseLibrary.DTOs;
+using ClientLibrary.Services.Contracts;
+
+namespace ClientLibrary.Services.Implementations
+{
+    public class UserAccountService : IUserAccountService
+    {
+        public Task<GeneralResponse> CreateAsync(Register user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponse> SignInAsync(Login user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponse> RefreshTokenAsync(RefreshToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<WeatherForecast[]> GetWeatherForecast()
+        {
+            throw new NotImplementedException();
+        }
+              
+    }
+}
