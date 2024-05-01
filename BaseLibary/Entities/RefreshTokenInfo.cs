9faba5d2210f5aeb5e982e3356421318ba281e76
mawalou14 <awaloum213@gmail.com>
2024-05-01 19:03:33 +0100

    Create a RefreshTokenInfo class entity

---
+++ BaseLibary/Entities/RefreshTokenInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace BaseLibary.Entities
+{
+    public class RefreshTokenInfo
+    {
+        public Guid Id { get; set; }
+        public string? Token { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
